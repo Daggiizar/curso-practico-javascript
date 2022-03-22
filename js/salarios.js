4f536586a67f203d55e8471f6fc488c36797471a
Daniel Zárate <d.ansizgz@gmail.com>
2022-03-22 15:18:42 -0500

    Añadí index.html con links al resto de ejercicios

---
+++ js/salarios.js
@@ -0,0 +1,181 @@
+const colombia = [];
+
+colombia.push({
+
+    name: "Daniel",
+    salary: 3000,
+
+});
+
+colombia.push({
+
+    name: "Alejandra",
+    salary: 8000,
+
+});
+
+colombia.push({
+
+    name: "Maria",
+    salary: 20000,
+
+});
+
+colombia.push({
+
+    name: "Guillermo",
+    salary: 4000,
+
+});
+
+colombia.push({
+
+    name: "Zarate",
+    salary: 2700,
+
+});
+
+colombia.push({
+
+    name: "Aguillon",
+    salary: 4000,
+
+});
+
+colombia.push({
+
+    name: "Guevara",
+    salary: 1000,
+
+});
+
+colombia.push({
+
+    name: "Marquez",
+    salary: 8000,
+
+});
+
+colombia.push({
+
+    name: "Pepo",
+    salary: 4400,
+
+});
+
+colombia.push({
+
+    name: "Pepito",
+    salary: 5700,
+
+});
+colombia.push({
+
+    name: "Pepon",
+    salary: 6000,
+
+});
+
+colombia.push({
+
+    name: "Peponote",
+    salary: 3000,
+
+});
+
+colombia.push({
+
+    name: "Señor x",
+    salary: 1000,
+
+});
+
+colombia.push({
+
+    name: "Señora x",
+    salary: 20000,
+
+});
+
+colombia.push({
+
+    name: "Fulanito",
+    salary: 3500,
+
+});
+colombia.push({
+
+    name: "Fulano",
+    salary: 4800,
+
+});
+
+colombia.push({
+
+    name: "Fulanita",
+    salary: 3500,
+
+});
+
+colombia.push({
+
+    name: "Fulana",
+    salary: 20000,
+
+});
+
+colombia.push({
+
+    name: "peter",
+    salary: 5000,
+
+});
+
+colombia.push({
+
+    name: "Anguila",
+    salary: 7800,
+
+});
+colombia.push({
+
+    name: "Castriti",
+    salary: 3000,
+
+});
+
+colombia.push({
+
+    name: "Josefo",
+    salary: 1000,
+
+});
+
+colombia.push({
+
+    name: "Josefa",
+    salary: 7000,
+
+});
+
+colombia.push({
+
+    name: "arquimedes",
+    salary: 5400,
+
+});
+
+colombia.push({
+
+    name: "Arqui",
+    salary: 2700,
+
+});
+
+colombia.push({
+
+    name: "Aquaman",
+    salary: 9000,
+
+});
+
